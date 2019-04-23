0889f755ef8311c2f22c4808df02196540d79eba
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-04-23 10:53:53 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { RouterModule } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { CalculationSheetComponent } from './calculation-sheet/calculation-sheet.component';
 import { FilterFormComponent } from './filter-form/filter-form.component';
-import { GraphQLModule } from './graphql.module';
 import { HeaderComponent } from './header/header.component';
 import { IssueManagerComponent } from './issue-manager/issue-manager.component';
-import { IssueTableComponent } from './issue-table/issue-table.component';
 import { PolicyComponent } from './policy/policy.component';
 import { SharedModule } from './shared/shared.module';
-import { SummaryComponent } from './summary/summary.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
 import { UserGuideComponent } from './user-guide/user-guide.component';
 
@@ -20,15 +16,12 @@ import { UserGuideComponent } from './user-guide/user-guide.component';
 @NgModule({
   declarations: [
     AppComponent,
-    IssueTableComponent,
     HeaderComponent,
     TutorialComponent,
     IssueManagerComponent,
     PolicyComponent,
     FilterFormComponent,
-    SummaryComponent,
     UserGuideComponent,
-    CalculationSheetComponent
   ],
   imports: [
     SharedModule,
@@ -40,7 +33,6 @@ import { UserGuideComponent } from './user-guide/user-guide.component';
   bootstrap: [AppComponent],
   entryComponents: [
     PolicyComponent,
-    CalculationSheetComponent
   ]
 })
 export class AppModule {}
